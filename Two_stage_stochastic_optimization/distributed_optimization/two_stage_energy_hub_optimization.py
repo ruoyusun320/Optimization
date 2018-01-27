649fed3817fbefd985dbad83a8ae26d3b0c15c22
Matrixeigs <matrixeigs@gmail.com>
2018-01-27 23:29:51 +0800

    Add two-stage stochastic optimization for energy hub

---
+++ Two_stage_stochastic_optimization/distributed_optimization/two_stage_energy_hub_optimization.py
@@ -0,0 +1,47 @@
+"""
+Two stage stochastic optimization problem for the hybrid AC/DC microgrid embedded energy hub
+@author: Tianyang Zhao
+@mail: zhaoty@ntu.edu.sg
+@date:27 Jan 2018
+"""
+from numpy import array
+from matplotlib import pyplot
+
+
+def problem_formulation(N, delta, weight_factor):
+    """
+    Jointed optimization for the electrical and thermal optimisation
+    :param N: number of scenario
+    :param delta: forecasting errors
+    :param weight_factor: weight factor between the first stage decision making and second stage decision makin
+    :return:
+    """
+
+    PHVDC_max = 50
+    # AC electrical demand
+    AC_PD = array([323.0284, 308.2374, 318.1886, 307.9809, 331.2170, 368.6539, 702.0040, 577.7045, 1180.4547, 1227.6240,
+                   1282.9344, 1311.9738, 1268.9502, 1321.7436, 1323.9218, 1327.1464, 1386.9117, 1321.6387, 1132.0476,
+                   1109.2701, 882.5698, 832.4520, 349.3568, 299.9920])
+    # DC electrical demand
+    DC_PD = array([287.7698, 287.7698, 287.7698, 287.7698, 299.9920, 349.3582, 774.4047, 664.0625, 1132.6996, 1107.7366,
+                   1069.6837, 1068.9819, 1027.3295, 1096.3820, 1109.4778, 1110.7039, 1160.1270, 1078.7839, 852.2514,
+                   791.5814, 575.4085, 551.1441, 349.3568, 299.992])
+    # Heating demand
+    HD = array([16.0996, 17.7652, 21.4254, 20.2980, 19.7012, 21.5134, 860.2167, 522.1926, 199.1072, 128.6201, 104.0959,
+                86.9985, 95.0210, 59.0401, 42.6318, 26.5511, 39.2718, 73.3832, 120.9367, 135.2154, 182.2609, 201.2462,
+                0, 0])
+    # Cooling demand
+    CD = array([16.0996, 17.7652, 21.4254, 20.2980, 19.7012, 21.5134, 860.2167, 522.1926, 199.1072, 128.6201, 104.0959,
+                86.9985, 95.0210, 59.0401, 42.6318, 26.5511, 39.2718, 73.3832, 120.9367, 135.2154, 182.2609, 201.2462,
+                0, 0])
+
+    pyplot.plot(AC_PD)
+    pyplot.ylabel('Electrical load profile')
+    pyplot.show()
+
+    model = {}
+    return model  # Formulated mixed integer linear programming problem
+
+
+if __name__ == "__main__":
+    model = problem_formulation(50, 0.03, 0)
