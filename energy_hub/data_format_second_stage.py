37735d54a9bde03ed430647f2a92116f65e11519
Matrixeigs <matrixeigs@gmail.com>
2018-02-12 22:18:35 +0800

    the second stage optimization problem data format

---
+++ energy_hub/data_format_second_stage.py
@@ -0,0 +1,14 @@
+# The data format for microgrid embedded energy hub
+GAS = 0  # CHP input
+UG = 1  # Utility grid input
+PAC2DC = 2  # Electrical energy from AC to DC
+PDC2AC = 3  # Electrical energy from AC to DC
+PHVAC = 4  # HVAC consumption
+EESS = 5  # ESS energy status
+PESSDC = 6  # ESS discharging rate
+PESSCH = 7  # ESS charging rate
+PH_POSITIVE = 8  # Positive derivation of heat supply
+PH_NEGATIVE = 9  # Positive derivation of heat supply
+PC_POSITIVE = 10  # Positive derivation of heat supply
+PC_NEGATIVE = 11  # Positive derivation of heat supply
+NX = 12  # Number of decision variables
