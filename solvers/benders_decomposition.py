451846b7d8bfd4dd1519f0b0905debae5e1514a9
Matrixeigs <matrixeigs@gmail.com>
2018-03-06 15:37:23 +0800

    remove bugs

--- solvers/benders_decomposition.py
+++ solvers/benders_decomposition.py
@@ -26,16 +26,34 @@ notes:
 5) The multi-cuts version Benders decomposition is adopted.
 """
 
-def main(c,A,b,ps,qs,Ws,hs,Ts):
+
+def main(c, A, b, Aeq, beq, vtype, ps, qs, Ws, hs, Ts):
     """
-    The standard
-    :param c:
-    :param A:
-    :param b:
-    :param ps:
-    :param qs:
-    :param Ws:
-    :param hs:
-    :param Ts:
-    :return: The obtained solution for
+    The standard input format for Benders decomposition problem
+    :param c: Cost parameter for the first stage optimization
+    :param A: Inequality constraint matrix for the first stage optimization
+    :param b: Inequality constraint parameters for the first stage optimization
+    :param Aeq: Equality constraint matrix for the first stage optimization
+    :param beq: Equality constraint parameters for the first stage optimization
+    :param vtype: The type for the first stage optimization problems
+    :param ps: Probability for the second stage optimization problem under scenario s
+    :param qs: Cost parameters for the second stage optimization problem
+    :param Ws: Equality constraint parameters for the second stage optimization
+    :param hs: Equality constraint parameters for the second stage optimization
+    :param Ts: Equality constraint matrix between the first stage and the second stage optimization
+    :return: The obtained solution for the first stage optimization
     """
+    # 1) Input check for the first stage optimization
+    
+    # 2) Reformulate the first stage optimization problem
+    # 2.1) Estimate the boundary of the first stage optimization problem.
+    # 2.2) Add additional variables to the first stage optimization problem
+
+    # 3) Reformulate the second stage optimization problem
+    # 3.1) Formulate the dual problem for each problem under dual problems
+
+    # 4) Begin the iteration
+
+    # 5) Check the output
+
+    # 6) Return the solution
--- unit_commitment/unti_commitment.py
+++ unit_commitment/unti_commitment.py
@@ -4,17 +4,32 @@ Basic unit commitment to some mix-integer linear/quadratic programming problem
 @e-mail: zhaoty@ntu.edu.sg
 @date:6 Mar 2018
 """
+from pypower.loadcase import loadcase
+from pypower.ext2int import ext2int
+from pypower.ext2int import ext2int
+from numpy import flatnonzero as find
+from scipy.sparse.linalg import inv
+from scipy.sparse import vstack
+from numpy import zeros, c_, shape, ix_,ones,r_,arange,sum,diag,concatenate
+
+from pypower.idx_brch import F_BUS, T_BUS, BR_X, TAP, SHIFT, BR_STATUS, RATE_A
+from pypower.idx_cost import MODEL, NCOST, PW_LINEAR, COST, POLYNOMIAL
+from pypower.idx_bus import BUS_TYPE, REF, VA, VM, PD, GS, VMAX, VMIN, BUS_I
+from pypower.idx_gen import GEN_BUS, VG, PG, QG, PMAX, PMIN, QMAX, QMIN
 
 
 def main(case):
     """
-
     :param case: The test case for unit commitment problem
     :return:
     """
     from unit_commitment.data_format import IG, PG
-
-
+    case = loadcase(case)
+    case = ext2int(case)
+    baseMVA, bus, gen, branch, gencost = case["baseMVA"], case["bus"], case["gen"], case["branch"], case["gencost"]  #
+    nb = shape(case['bus'])[0]  # number of buses
+    nl = shape(case['branch'])[0]  # number of branches
+    ng = shape(case['gen'])[0]  # number of schedule injections
 
 
     return model
