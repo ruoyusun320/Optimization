5a1a17baf08e2630f4a7464caad6e6600afee3d0
Matrixeigs <matrixeigs@gmail.com>
2018-03-06 08:47:19 +0800

    Input and output of Benders decomposition

--- solvers/benders_decomposition.py
+++ solvers/benders_decomposition.py
@@ -1,7 +1,41 @@
 """
-Benders decomposition for
-It should be noted as follows
-1) The dual problem is solved.
-2) A multi-cuts version of framework is adopted to solve the problem
+Benders decomposition method for two-stage stochastic optimization problems
+    Minimize a function F(X) beginning, subject to
+	optional linear and nonlinear constraints and variable bounds:
+			min  c'*x + sum(p_s*Q_s(x))
+			x
+			s.t. A*x<=b,
+			     Aeq*x==beq,   x \in [lb,ub]
+			where Q_s(x)=min q_s'*y
+			             y
+			             s.t. W_s*y = h_s-T_s*x
+			             y \in R^+
+References:
+    [1]Benders Decomposition for Solving Two-stage Stochastic Optimization Models
+    https://www.ima.umn.edu/materials/2015-2016/ND8.1-12.16/25378/Luedtke-spalgs.pdf
+@author: Tianyang Zhao
+@e-mail: zhaoty@ntu.edu.sg
+@date: 8 Feb 2018
+@version: 0.1
+
+notes:
+1) The data structure is based on the numpy and scipy
+2) This algorithm should be extended for further version to solve the jointed chance constrained stochastic programming
+3) In this test algorithm, Mosek is adpoted. https://www.mosek.com/
+4) In the second stage optimization, the dual problem is solved, so that only the gurobi problem is needed to solve the problem.
+5) The multi-cuts version Benders decomposition is adopted.
 """
 
+def main(c,A,b,ps,qs,Ws,hs,Ts):
+    """
+    The standard
+    :param c:
+    :param A:
+    :param b:
+    :param ps:
+    :param qs:
+    :param Ws:
+    :param hs:
+    :param Ts:
+    :return: The obtained solution for
+    """
