bc14e206ccf26dd9194f6dc6c0f348e9a85ecf4c
Matrixeigs <matrixeigs@gmail.com>
2018-03-06 10:41:27 +0800

    Add unit commitment package

---
+++ unti_commitment/__init__.py
@@ -0,0 +1,7 @@
+"""
+Unit commitment problems for power systems, including the following models.
+1) Unit commitment model without transmission lines (For the development of branch and bound method)
+2) Unit commitment model with
+3) Security constrained unit commitment
+4)
+"""
