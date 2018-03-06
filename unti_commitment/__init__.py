74617acb7bf7578a90d2bd64ecd8b12b8de2a2ed
Matrixeigs <matrixeigs@gmail.com>
2018-03-06 10:48:21 +0800

    Function developments

--- unti_commitment/__init__.py
+++ unti_commitment/__init__.py
@@ -1,7 +1,11 @@
 """
 Unit commitment problems for power systems, including the following models.
 1) Unit commitment model without transmission lines (For the development of branch and bound method)
-2) Unit commitment model with
+2) Unit commitment model with transmission works
 3) Security constrained unit commitment
-4)
+4) Price based unit commitment
+5) Two-stage unit commitment with recourse
+@author:Zhao Tianyang
+@e-mail:zhaoty@ntu.edu.sg
+
 """
