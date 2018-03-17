5a21415c8a1765648fdd9536b1db2dec9d3a128f
Matrixeigs <matrixeigs@gmail.com>
2018-03-17 14:15:09 +0800

    Add data format

--- unit_commitment/unti_commitment.py
+++ unit_commitment/unti_commitment.py
@@ -6,16 +6,11 @@ Basic unit commitment to some mix-integer linear/quadratic programming problem
 """
 from pypower.loadcase import loadcase
 from pypower.ext2int import ext2int
-from pypower.ext2int import ext2int
 from numpy import flatnonzero as find
 from scipy.sparse.linalg import inv
 from scipy.sparse import vstack
 from numpy import zeros, c_, shape, ix_,ones,r_,arange,sum,diag,concatenate
 
-from pypower.idx_brch import F_BUS, T_BUS, BR_X, TAP, SHIFT, BR_STATUS, RATE_A
-from pypower.idx_cost import MODEL, NCOST, PW_LINEAR, COST, POLYNOMIAL
-from pypower.idx_bus import BUS_TYPE, REF, VA, VM, PD, GS, VMAX, VMIN, BUS_I
-from pypower.idx_gen import GEN_BUS, VG, PG, QG, PMAX, PMIN, QMAX, QMIN
 
 
 def main(case):
@@ -24,9 +19,7 @@ def main(case):
     :return:
     """
     from unit_commitment.data_format.data_format import IG, PG
-    case = loadcase(case)
-    case = ext2int(case)
-    baseMVA, bus, gen, branch, gencost = case["baseMVA"], case["bus"], case["gen"], case["branch"], case["gencost"]  #
+    baseMVA, bus, gen, branch, profile = case["baseMVA"], case["bus"], case["gen"], case["branch"],case["Load_profile"]
     nb = shape(case['bus'])[0]  # number of buses
     nl = shape(case['branch'])[0]  # number of branches
     ng = shape(case['gen'])[0]  # number of schedule injections
@@ -37,7 +30,7 @@ def main(case):
 
 
 if __name__ == "__main__":
-    from pypower import case118
+    from unit_commitment.test_cases import case118
 
     test_case = case118.case118()
     model = main(test_case)
